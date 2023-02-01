f7ae3ab6166e02cf14393c948ed56d5c584a48f9
k-trzcionka <klaudiusz.trzcionka@student.wsb.edu.pl>
2023-02-01 19:38:22 +0100

    Added test for discriminant equal 0

--- QuadraticEquationTests/UnitTest1.cs
+++ QuadraticEquationTests/UnitTest1.cs
@@ -32,5 +32,21 @@ namespace QuadraticEquation.Tests
             // Assert
             Assert.Equal(0, numberOfRoots);
         }
+
+        [Theory]
+        [InlineData(1, -4, 4, 2, 2)]
+        [InlineData(2, -8, 8, 2, 2)]
+        [InlineData(1, -2, 1, 1, 1)]
+        public void CalculateRoots_OneRootTest(double a, double b, double c, double expectedX1, double expectedX2)
+        {
+            // Act
+            double x1, x2;
+            int numberOfRoots = Program.CalculateRoots(a, b, c, out x1, out x2);
+
+            // Assert
+            Assert.Equal(1, numberOfRoots);
+            Assert.Equal(expectedX1, x1);
+            Assert.Equal(expectedX2, x2);
+        }
     }
 }
