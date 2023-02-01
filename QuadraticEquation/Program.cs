8071a040df224dcac922fe8f3d6a932b2486adf9
k-trzcionka <klaudiusz.trzcionka@student.wsb.edu.pl>
2023-02-01 18:04:46 +0100

    Get user input and define variables

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quadratic equation: ax^2 + bx + c = 0");
+            Console.WriteLine("Enter a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter c:");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = b * b - 4 * a * c;
+            double x1, x2;
+
+
+        }
+    }
+}
