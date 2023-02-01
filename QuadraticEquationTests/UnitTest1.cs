fc9135b8a143bcf3f5e5c415c2e9dfd8d0bc49f4
k-trzcionka <klaudiusz.trzcionka@student.wsb.edu.pl>
2023-02-01 19:33:52 +0100

    Added test for positive discriminant

--- QuadraticEquationTests/UnitTest1.cs
+++ QuadraticEquationTests/UnitTest1.cs
@@ -1,1 +1,22 @@
 using QuadraticEquation;
+
+namespace QuadraticEquation.Tests
+{
+    public class ProgramTests
+    {
+        [Theory]
+        [InlineData(1, -3, 2, 2, 1)]
+        [InlineData(1, 5, 6, -2, -3)]
+        [InlineData(2, 7, 6, -1.5, -2)]
+        public void CalculateRoots_PositiveTests(double a, double b, double c, double expectedX1, double expectedX2)
+        {
+            // Act
+            double x1, x2;
+            int numberOfRoots = Program.CalculateRoots(a, b, c, out x1, out x2);
+            // Assert
+            Assert.Equal(2, numberOfRoots);
+            Assert.Equal(expectedX1, x1);
+            Assert.Equal(expectedX2, x2);
+        }
+    }
+}
