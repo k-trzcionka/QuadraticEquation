29b0a2a89e16386cf1308430552ae18f6634b43c
k-trzcionka <klaudiusz.trzcionka@student.wsb.edu.pl>
2023-02-01 18:24:29 +0100

    Add reference test>program and set 'Program' class 'Public'

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -2,7 +2,7 @@
 
 namespace QuadraticEquation
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
--- QuadraticEquationTests/UnitTest1.cs
+++ QuadraticEquationTests/UnitTest1.cs
@@ -1,11 +1,1 @@
-namespace QuadraticEquationTests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
-
-        }
-    }
-}
+using QuadraticEquation;
