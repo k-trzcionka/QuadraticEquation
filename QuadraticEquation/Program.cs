08dca637c92c29a9b960d160e7224f0d06036eb0
k-trzcionka <klaudiusz.trzcionka@student.wsb.edu.pl>
2023-02-01 18:08:28 +0100

    Do the calculations and print the result

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -17,6 +17,26 @@ namespace QuadraticEquation
             double delta = b * b - 4 * a * c;
             double x1, x2;
 
+            if (delta > 0)
+            {
+                x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                Console.WriteLine("The roots are real and different.");
+                Console.WriteLine("x1 = {0}, x2 = {1}", x1, x2);
+            }
+            else if (delta == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine("The roots are real and same.");
+                Console.WriteLine("x1 = x2 = {0}", x1);
+            }
+            else
+            {
+                Console.WriteLine("The roots are complex and different.");
+            }
+
+            Console.ReadKey();
+
 
         }
     }
